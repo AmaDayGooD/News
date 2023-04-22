f81e5df68ecaee79cf783d541b8e638496cbfc16
Aleksandr <sasha.schibaew@yandex.ru>
2023-04-22 10:50:11 +0300

    Harry 22.04.2023

--- app/src/main/java/com/example/news/MainActivity.kt
+++ app/src/main/java/com/example/news/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.news
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.news.databinding.ActivityMainBinding
 import com.example.news.retrofit.FactsAPI
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -13,46 +16,49 @@ import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private  var launcher : ActivityResultLauncher<Intent>? = null
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val tv = findViewById<TextView>(R.id.textView2)
-        val btn = findViewById<Button>(R.id.button3)
-        val enrt = findViewById<TextView>(R.id.editTextNumber2)
-        val rnd = findViewById<Button>(R.id.rand)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result: ActivityResult ->
+            if(result.resultCode == RESULT_OK)
+            {
+                val text = result.data?.getStringExtra("key1")
+            }
+        }
         val retrofit = Retrofit.Builder()
             .baseUrl("https://harry-potter-api-en.onrender.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val factsAPI =  retrofit.create(FactsAPI::class.java)
 
-        rnd.setOnClickListener {
+        binding.randbtn.setOnClickListener {
 
             val randomNumber = Random.nextInt(73)
-            enrt.setText(randomNumber.toString())
-
+            binding.edtext.setText(randomNumber.toString())
 
                 CoroutineScope(Dispatchers.IO).launch {
                     val apis = factsAPI.getFact(randomNumber.toString().toInt())
                     runOnUiThread {
-                        tv.text = apis.spell
+                        binding.textView2.text = apis.spell
                     }
                 }
         }
-
-
-        btn.setOnClickListener{
-            if(enrt.text.isNotEmpty() && (enrt.text.toString().toInt() in (1..72)))
+            binding.button3.setOnClickListener{
+            if(binding.edtext.text.isNotEmpty() && (binding.edtext.text.toString().toInt() in (1..72)))
             {
             CoroutineScope(Dispatchers.IO).launch {
-                val apis = factsAPI.getFact(enrt.text.toString().toInt())
+                val apis = factsAPI.getFact(binding.edtext.text.toString().toInt())
                 runOnUiThread {
-                    tv.text = apis.spell
+                    binding.textView2.text = apis.spell
                 }
             }
         }
-            else tv.text = "Enter another number!"
+            else binding.textView2.text = "Enter another number!"
         }
 
 
